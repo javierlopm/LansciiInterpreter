8ac66cf28cc00703dff9a8a4045c0188843ae335
Javier Lopez <javierlopm@hotmail.com>
2015-04-22 11:13:09 -0430

    Commit inicial

---
+++ lexer/findRegex.rb
@@ -0,0 +1,55 @@
+class FindRegex
+
+    @regexDic
+
+    @MAYBETOKEN = [ 
+        /\{/,
+        /\}/,
+        /|/,
+        /%/,
+        /!/,
+        /@/,
+        /=/,
+        /;/,
+        /read/,
+        /write/,
+        /\?/,
+        /:/,
+        /[/,
+        /]/,
+        /\+/,
+        /-/,
+        /\*/,
+        /\//,
+        /%/,
+        /true/,
+        /false/,
+        /\/\\/,
+        /\\\//,
+        /^/,
+        /</,
+        /<=/,
+        />/,
+        />=/,
+        /\/=/,
+        /<>/, #Expresion para lienzo
+        //,#Expresion para entero
+        //,#Expresion booleana
+        //,
+        //,
+        //,
+        //,
+        //,
+        //,
+    ]
+
+    def initialize(args)
+        
+    end
+
+    def findAll(program)
+
+    end
+    
+    
+end
