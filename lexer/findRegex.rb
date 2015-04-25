4a04a25eb8c484041cbb55e74a640e63a3a08da6
Javier Lopez <javierlopm@hotmail.com>
2015-04-24 21:42:52 -0430

    Nuevas expresiones regulares, nombres de tokens agregados

--- lexer/findRegex.rb
+++ lexer/findRegex.rb
@@ -3,10 +3,10 @@ class FindRegex
     @regexDic
 
     @MAYBETOKEN = [ 
-        /\{/,
-        /\}/,
+        /\{(?!\-)/,
+        /(?<!\-)\}/,
         /|/,
-        /%/,
+        /%/,            #Esta declaracion no debe tener ajuro un identificador despues?
         /!/,
         /@/,
         /=/,
@@ -21,7 +21,7 @@ class FindRegex
         /-/,
         /\*/,
         /\//,
-        /%/,
+        /\d+%\d+/,        #Creo que para que sea modulo debe tener digito antes y despues
         /true/,
         /false/,
         /\/\\/,
@@ -32,15 +32,67 @@ class FindRegex
         />/,
         />=/,
         /\/=/,
-        /<>/, #Expresion para lienzo
-        //,#Expresion para entero
-        //,#Expresion booleana
-        //,
-        //,
-        //,
-        //,
-        //,
-        //,
+        #/<>/, #Expresion para lienzo
+        #//,#Expresion para entero
+        #//,#Expresion booleana
+        /[a-zA-Z]\w*/, #Identificador
+        /#/,
+        /<.*\/.*>/,
+        /<.*\\.*>/,
+        /<.*\-.*>/,
+        /<.*_.*>/,
+        /'/,        #Transposicion              --Operador mas fuerte
+        /$(<[|/\\\-_\ ]*>|#)/,        #Rotacion, no se si aceptar tambien un identificador...
+        //,         #Concatenacion horizontal
+        //,         #Concatenacion vertical
+        /\{\-/,
+        /\-\}/,
+        /\.\./,
+    ]
+
+    @TOKENNAME = [
+        "LCURLY",
+        "RCURLY",
+        "PIPE",
+        "PERCENT",
+        "EXCLAMATION MARK",
+        "AT",
+        "EQUALS",
+        "SEMICOLON",
+        "READ",
+        "WRITE",
+        "QUESTIONMARK",
+        "COLON",
+        "LSQUARE",
+        "RSQUARE",
+        "PLUS",
+        "MINUS",
+        "MULTIPLICATION SIGN",  #???
+        "SLASH",                #???
+        "MODULO",
+        "TRUE",
+        "FALSE",
+        "AND",
+        "OR",
+        "NOT",
+        "LESS",
+        "LESSTHAN",
+        "MORE",
+        "MORETHAN",
+        "NOTEQUALS",
+        "IDENTIFIER",
+        "EMPTY CANVAS",
+        "CANVAS",
+        "CANVAS",
+        "CANVAS",
+        "CANVAS",
+        "TRANSPOSE",
+        "ROTATION",
+        "HORIZONTALCAT",    #???????
+        "VERTICALCAT",      #???????
+        "LCOMMENT",
+        "RCOMMENT",
+        "COMPREHENSION"     #??????
     ]
 
     def initialize(args)
