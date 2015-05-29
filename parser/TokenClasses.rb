5c8afbaed4a8284789fe48997e9fb36e7a206d5d
“Patricia“ <patricia.reinoso12@gmail.com>
2015-05-29 08:20:57 -0430

    Expresiones

--- parser/TokenClasses.rb
+++ parser/TokenClasses.rb
@@ -9,146 +9,213 @@
 type = 0 ==> Aritmetic 
 type = 1 ==> Boolean
 type = 2 ==> Canvas
+type = 3 ==> Cualquier tipo
 
 =end
+
+# Expresiones binarias
+
 class BinExpr
 
+	def initialize (subexpr1, subexpr2)
 
-class ExprSum
+		@subexpr1 = subexpr1
+		@subexpr2 = subexpr2
 
-	def initialize(subexpr1, subexpr2)
-
-	@subexpr1 = subexpr1
-	@subexpr2 = subexpr2
-	@op = "+"
-	@type = 0
-
-class ExprSubs
+class ExprSum < BinExpr
 
 	def initialize(subexpr1, subexpr2)
 
-	@subexpr1 = subexpr1
-	@subexpr2 = subexpr2
-	@op = "-"
-	@type = 0
+		@op = "+"
+		@type = 0
 
-class ExpreMult
+class ExprSubs < BinExpr
 
 	def initialize(subexpr1, subexpr2)
 
-	@subexpr1 = subexpr1
-	@subexpr2 = subexpr2
-	@op = "*"
-	@type = 0
+		@op = "-"
+		@type = 0
 
-class ExprDiv
+class ExpreMult < BinExpr
 
 	def initialize(subexpr1, subexpr2)
 
-	@subexpr1 = subexpr1
-	@subexpr2 = subexpr2
-	@op = "/"
-	@type = 0
+		@op = "*"
+		@type = 0
 
-class ExprMod
+class ExprDiv < BinExpr
 
 	def initialize(subexpr1, subexpr2)
 
-	@subexpr1 = subexpr1
-	@subexpr2 = subexpr2
-	@op = "%"
-	@type = 0
+		@op = "/"
+		@type = 0
 
-class ExprAnd
+class ExprMod < BinExpr
 
 	def initialize(subexpr1, subexpr2)
 
-	@subexpr1 = subexpr1
-	@subexpr2 = subexpr2
-	@op = "/\\"
-	@type = 1
+		@op = "%"
+		@type = 0
 
-
-class ExprOr
+class ExprAnd < BinExpr
 
 	def initialize(subexpr1, subexpr2)
 
-	@subexpr1 = subexpr1
-	@subexpr2 = subexpr2
-	@op = "\/"
-	@type = 1
+		@op = "/\\"
+		@type = 1
 
-class ExprLess
+class ExprOr < BinExpr
 
 	def initialize(subexpr1, subexpr2)
 
-	@subexpr1 = subexpr1
-	@subexpr2 = subexpr2
-	@op = "<"
-	@type = 1
+		@op = "\/"
+		@type = 1
 
-class ExprLessEql
+class ExprLess < BinExpr
 
 	def initialize(subexpr1, subexpr2)
 
-	@subexpr1 = subexpr1
-	@subexpr2 = subexpr2
-	@op = "<="
-	@type = 1
+		@op = "<"
+		@type = 1
 
-class ExprMore
+class ExprLessEql < BinExpr
 
 	def initialize(subexpr1, subexpr2)
 
-	@subexpr1 = subexpr1
-	@subexpr2 = subexpr2
-	@op = ">"
-	@type = 1
+		@op = "<="
+		@type = 1
 
-class ExprMoreEql
+class ExprMore < BinExpr
 
 	def initialize(subexpr1, subexpr2)
 
-	@subexpr1 = subexpr1
-	@subexpr2 = subexpr2
-	@op = ">="
-	@type = 1
+		@op = ">"
+		@type = 1
 
-class ExprEql
+class ExprMoreEql < BinExpr
 
 	def initialize(subexpr1, subexpr2)
 
-	@subexpr1 = subexpr1
-	@subexpr2 = subexpr2
-	@op = "="
-	@type = 1
+		@op = ">="
+		@type = 1
 
-class ExprDiff
+class ExprEql < BinExpr
 
 	def initialize(subexpr1, subexpr2)
 
-	@subexpr1 = subexpr1
-	@subexpr2 = subexpr2
-	@op = "/="
-	@type = 1
+		@op = "="
+		@type = 1
 
-class ExprVerConcat
+class ExprDiff < BinExpr
+
+	def initialize(subexpr1, subexpr2)
+
+		@op = "/="
+		@type = 1
+
+class ExprVerConcat < BinExpr
 	
 	def initialize(subexpr1, subexpr2)
 
-	@subexpr1 = subexpr1
-	@subexpr2 = subexpr2
-	@op = "&"
-	@type = 2
+		@op = "&"
+		@type = 2
 
-class ExprHorConcat
+class ExprHorConcat < BinExpr
 
 	def initialize(subexpr1, subexpr2)
 
-	@subexpr1 = subexpr1
-	@subexpr2 = subexpr2
-	@op = "~"
-	@type = 2
+		@op = "~"
+		@type = 2
+
+
+# Expresiones unarias
+
+class UnExpr 
+
+class ExprUnMinus < UnExpr
+
+	def initialize(subexpr1)
+
+		@op = "-"
+		@type = 0
+
+class ExprNot < UnExpr
+
+	def initialize(subexpr1)
+
+		@op = "^"
+		@type = 1
+
+class ExprTranspose < UnExpr
+
+	def initialize(subexpr1)
+
+		@op = "'"
+		@type = 2
+
+# Expresiones constantes
+
+class ExprParenthesis
+	
+	def initialize(subexpr1)
+
+		@lp = "("
+		@rp = ")"
+		@subexpr1 = subexpr1
+		@type = 3
+
+class ExprNumber
+
+	def initialize(subexpr1)
+
+		@subexpr1 = subexpr1
+		@type = 0
+
+class ExprTrue
+
+	def initialize()
+
+		@subexpr1 = "true"
+		@type = 1
+
+class ExprFalse
+
+	def initialize()
+
+		@subexpr1 = "false"
+		@type = 1
+
+class ExprId
+
+	def initialize(identifier)
+
+		@subexpr1 = identifier
+		@type = 3
+
+class ExprCanvas
+
+	def initialize(canvas)
+
+		@subexpr1 = canvas
+		@type = 3
+
+class ExprEmptyCanvas
+
+	def initialize()
+
+		@subexpr1 = "#"
+		@type = 2
+
+
+
+
+
+
+
+
+
+
+
 
 
 
