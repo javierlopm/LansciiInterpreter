e8d22a57dc770ec3415d40abe4cb7371912e61e4
“Patricia“ <patricia.reinoso12@gmail.com>
2015-06-11 11:41:01 -0430

    Comienzos de estructura de arbol

--- dataStructures/symbolTable.rb
+++ dataStructures/symbolTable.rb
@@ -1,10 +1,11 @@
 class SymbolTable
-  def initialize
+  def initialize(father = nil)
     @tb = {}
+    @father = father
     @errors = []
   end
 
-  def insert(identifier,content)
+  def insert_symbol(identifier,content)
     if contains?identifier
       @errors << "identifier #{identifier} already declared"
     else
@@ -12,20 +13,45 @@ class SymbolTable
     end
   end
 
-  def delete(identifier)
-    @tb.delete(identifier)
-  end
+  def delete_symbol(identifier) #el nombre se confundia
+    if @tb.has_key?(identifier) then
+       @tb.delete(identifier)
+    elsif @father.nil? then
+      #skip NO SE QUE PONER AQUI 
+    else 
+      @father.delete_symbol(identifier)
+    end  
+  end #y si nunca lo consigue?! ERROR ??
 
-  def update(identifier,content)
-    @tb[identifier] = content
+  def update_symbol(identifier,content)
+    if @tb.has_key?(identifier) then
+       @tb[identifier] = content
+    elsif @father.nil? then
+      #skip NO SE QUE PONER AQUI
+    else 
+      @father.update_symbol(identifier)
+    end
+    
   end
 
   def contains?(identifier)
-    res = @tb.has_key?identifier
+    if @father.nil? then
+      return @tb.has_key?(identifier)
+    else
+      return (@tb.has_key?(identifier) or @father.contains?(identifier))
+    end
+    #res = @tb.has_key?identifier
   end
 
   def lookup(identifier)
-    res = @tb[identifier]
+    if @tb.has_key?(identifier) then
+      return @tb[identifier]
+    elsif @father.nil? then
+      return nil
+    else
+      @father.lookup(identifier)
+    end
+    #res = @tb[identifier]
   end
 
 end
