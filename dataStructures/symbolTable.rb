ea041723c17fac9aa99e114bd97598a9fc572d68
“Patricia“ <patricia.reinoso12@gmail.com>
2015-06-20 15:05:57 -0430

    Analsisis TP4

--- dataStructures/symbolTable.rb
+++ dataStructures/symbolTable.rb
@@ -35,11 +35,11 @@ class SymbolTable
     @errors << error
   end
 
-  def insert_symbol(identifier,type,modifiable=true)
+  def insert_symbol(identifier,type,modifiable=true,value=nil)
     if contains?identifier
       @errors.add_error(ReDeclared::new(identifier))
     else
-      @tb[identifier] = {'type' => type, 'modifiable' => modifiable}
+      @tb[identifier] = {'type' => type, 'modifiable' => modifiable, 'value' => value}
     end
   end
 
